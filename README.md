4ae0167a627d45d0b50e4e1a954ef32618758f5e
Abu Abdullah <abdullah72308@gmail.com>
2023-04-19 09:57:55 +0600

    responsive updated

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-Live link: https://ema-jhon-firebase-a234a.web.app
 =======
 project live link: 'https://ema-jhon-firebase-a234a.web.app/'
 >>>>>>> 786580f180e62f8017ff9e17d6bfbce55c255aa2
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,14 @@
 import React, { useContext, useState } from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
     const { user, signOutUser } = useContext(AuthContext);
-    const location = useLocation();
 
     const handleSignOut = () => {
         signOutUser()
@@ -47,14 +45,17 @@ const Header = () => {
                                     <Link onClick={handleOpen} to="/">Shop</Link>
                                     <Link onClick={handleOpen} to="/orders">Order</Link>
                                     <Link onClick={handleOpen} to="/inventory">Inventory</Link>
-                                    <Link onClick={handleOpen} to="/login">Login</Link>
+                                    <Link onClick={handleOpen} to="/login">Log In</Link>
+                                    {
+                                        user && <Link onClick={handleSignOut}>Sign Out</Link>
+                                    }
                                 </div>
                             )
                         }
                     </span>
                     </div>
                     <img src={logo} alt="" />
-                    <div>
+                    <div className='user-profile'>
                     {
                         user && <div><p className='email-text'>{user.email}</p> <button onClick={handleSignOut} id='sign-out'>Sign Out</button></div>
                     }
